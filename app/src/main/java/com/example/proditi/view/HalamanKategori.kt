c48be93fa6b239885118085f885dbac70603366f
ZakyMalika <zakymalika0987@gmail.com>
2026-01-16 22:43:18 +0700

    fix home and create kategori

--- app/src/main/java/com/example/proditi/view/HalamanKategori.kt
+++ app/src/main/java/com/example/proditi/view/HalamanKategori.kt
@@ -7,7 +7,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,6 +32,9 @@ fun HalamanKategoriHome(
     modifier: Modifier = Modifier,
     viewModel: KategoriHomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    LaunchedEffect (Unit) {
+        viewModel.getKategori()
+    }
     Scaffold(
         topBar = {
             ProdiTITopAppBar(
@@ -76,7 +84,13 @@ fun HalamanKategoriEntry(
     viewModel: KategoriEntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // State untuk Snackbar
+
+    var isError by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+
     Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }, // Pasang Host
         topBar = {
             ProdiTITopAppBar(
                 title = "Tambah Kategori",
@@ -93,15 +107,32 @@ fun HalamanKategoriEntry(
         ) {
             OutlinedTextField(
                 value = viewModel.uiState.namaKategori,
-                onValueChange = { viewModel.updateUiState(viewModel.uiState.copy(namaKategori = it)) },
+                onValueChange = {
+                    viewModel.updateUiState(viewModel.uiState.copy(namaKategori = it))
+                    isError = false // Hilangkan error saat user mengetik ulang
+                },
                 label = { Text("Nama Kategori") },
+                isError = isError, // Tampilkan merah jika error
+                supportingText = { // Pesan error di bawah kolom
+                    if (isError) {
+                        Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
+                    }
+                },
                 modifier = Modifier.fillMaxWidth()
             )
+
             Button(
                 onClick = {
-                    coroutineScope.launch {
-                        viewModel.saveKategori()
-                        navigateBack()
+                    if (viewModel.uiState.namaKategori.isBlank()) {
+                        isError = true
+                        errorMessage = "Nama Kategori tidak boleh kosong!"
+                    } else {
+                        coroutineScope.launch {
+                            viewModel.saveKategori()
+                            snackbarHostState.showSnackbar("Data berhasil ditambahkan")
+                            kotlinx.coroutines.delay(100)
+                            navigateBack()
+                        }
                     }
                 },
                 modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
